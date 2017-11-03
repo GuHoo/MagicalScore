bed2e47a4f07007607ff5991372814a0359eeab8
Nakai Tomoki <s16g459@stu.kagawa-u.ac.jp>
2017-11-03 20:06:19 +0900

    視線とオブジェクトの連動

--- Assets/Scripts/MagicalScoreManager.cs
+++ Assets/Scripts/MagicalScoreManager.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class MagicalScoreManager : MonoBehaviour {
 
 	public MoverioUnityPlugin moverioUnityPlugin;
 
-	void Start () {
+	public GameObject camera;
+	public GameObject scrole;
 
+	public Text x;
+	public Text y;
+	public Text z;
+	//public Text w;
+
+	bool hasFixedJoint = true;
+
+	void Start () {
+		Input.gyro.enabled = true;
+		Debug.Log (scrole.GetComponent<FixedJoint> ().connectedBody);
 	}
 
 	void OnDisable(){
@@ -19,6 +31,17 @@ public class MagicalScoreManager : MonoBehaviour {
 
 	void Update () {
 		DownKeyCheck ();
+		/*
+		x.text = Input.gyro.attitude.x.ToString();
+		y.text = Input.gyro.attitude.y.ToString();
+		z.text = Input.gyro.attitude.z.ToString();
+		w.text = Input.gyro.attitude.w.ToString();
+		*/
+
+
+		x.text = camera.transform.eulerAngles.x.ToString ();
+		y.text = camera.transform.eulerAngles.y.ToString ();
+		z.text = camera.transform.eulerAngles.z.ToString ();
 	}
 
 	/// <summary>
@@ -29,16 +52,28 @@ public class MagicalScoreManager : MonoBehaviour {
 		// 真ん中のボタン
 		if (Input.GetButtonDown("Fire1")) {
 			PushEnterKey ();
-		} else if (Input.GetKey(KeyCode.UpArrow)) {
+		} else if (Input.GetKey (KeyCode.UpArrow)) {
 			PushUpKey ();
-		} else if (Input.GetKey(KeyCode.DownArrow)) {
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
 			PushDownKey ();
-		} else if (Input.GetKey(KeyCode.LeftArrow)) {
+		} else if (Input.GetKey (KeyCode.LeftArrow)) {
 			PushLeftKey ();
-		} else if (Input.GetKey(KeyCode.RightArrow)) {
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
 			PushRightKey ();
-		} else if (Input.GetKey(KeyCode.Escape)) {
+		} else if (Input.GetKey (KeyCode.Escape)) {
 			PushEscapeKey ();
+		}
+	}
+
+	private void PushEnterKey ()
+	{
+		if (hasFixedJoint) {
+			Destroy (scrole.GetComponent<FixedJoint> ());
+			hasFixedJoint = false;
+		} else {
+			scrole.AddComponent<FixedJoint> ();
+			scrole.GetComponent<FixedJoint> ().connectedBody = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Rigidbody>();
+			hasFixedJoint = true;
 		}
 	}
 
@@ -61,11 +96,7 @@ public class MagicalScoreManager : MonoBehaviour {
 	{
 
 	}
-
-	private void PushEnterKey ()
-	{
 		
-	}
 
 	private void PushEscapeKey (){
 
