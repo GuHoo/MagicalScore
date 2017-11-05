38290574a02cd39539e1587ad99479546a569482
Nakai Tomoki <s16g459@stu.kagawa-u.ac.jp>
2017-11-05 16:32:10 +0900

    開始時に暗闇になるスクリプト

---
+++ Assets/Scripts/WelcomeDarkness.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WelcomeDarkness : MonoBehaviour {
+
+	void Start () {
+		transform.rotation =  Quaternion.Euler(270.0f, 0.0f, 0.0f);
+	}
+
+	void Update () {
+		
+	}
+}
