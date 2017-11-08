4b611460fdaafdd9a09bc3b5d265aaf1866f74ae
Nakai Tomoki <s16g459@stu.kagawa-u.ac.jp>
2017-11-08 13:49:07 +0900

    起動時に楽譜の一覧を表示

--- Assets/Scripts/UGUIController.cs
+++ Assets/Scripts/UGUIController.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using UnityEngine.UI;
 
 public class UGUIController : MonoBehaviour {
 
 	public GameObject uGUI;
+	public GameObject toggleScrollTitle;
+	public GameObject content;
 
 	void Start () {
-		
+		SetScoreTitle ();
 	}
 
 	void Update () {
@@ -19,26 +22,21 @@ public class UGUIController : MonoBehaviour {
 		uGUI.SetActive (false);
 	}
 
-	public void CheckStoragePath () {
-		#if !UNITY_EDITOR && UNITY_ANDROID
-			using (AndroidJavaClass environment = new AndroidJavaClass("android.os.Environment"))
-			{
-				using (AndroidJavaObject storageDir = environment.CallStatic<AndroidJavaObject>("getExternalStorageDirectory"))
-				{
-					string _StorageDir = storageDir.Call<string>("getCanonicalPath");
-					using (AndroidJavaClass log = new AndroidJavaClass("android.util.Log"))
-					{
-						log.CallStatic<int>("i", "UnityApp", _StorageDir);
-					}
-				}
-			}
-		#endif
+	public void SetScoreTitle () {
+		GameObject toggleScrollTitleInstance;
+		DirectoryInfo directoryInfo = new DirectoryInfo(Application.persistentDataPath + "/Score");
+		for(int i = 0; i < directoryInfo.GetDirectories ().Length; i++) {
+			DirectoryInfo tmpDirInfo = directoryInfo.GetDirectories () [i];
+			toggleScrollTitleInstance = Instantiate(toggleScrollTitle);
+			toggleScrollTitleInstance.GetComponentInChildren<Text>().text = tmpDirInfo.Name;
+			toggleScrollTitleInstance.transform.SetParent(content.transform);
+		}
+
 	}
 
 	public void GetScoreImage () {
 		DirectoryInfo directoryInfo = new DirectoryInfo(Application.persistentDataPath + "/Score");
 
-		Debug.Log (directoryInfo.GetDirectories ().Length);
 		for(int i = 0; i < directoryInfo.GetDirectories ().Length; i++) { // Score下のディレクトリ
 			DirectoryInfo tmpDirInfo = directoryInfo.GetDirectories () [i];
 			for(int j = 0; j < tmpDirInfo.GetFiles().Length; j++) { // Scoreの中のファイル
