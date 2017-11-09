a74f7e1ed291a6911f6a7ababc290ee5acddcff6
Nakai Tomoki <s16g459@stu.kagawa-u.ac.jp>
2017-11-09 14:38:17 +0900

    使ってない処理削除

--- Assets/BookG/BookG.cs
+++ Assets/BookG/BookG.cs
@@ -5,12 +5,9 @@ using UnityEngine;
 public class BookG : MonoBehaviour {
     private Animator animator;
     //int blendShapeCount;
-    SkinnedMeshRenderer smr;
-
 
     float blendOne = 0f;
     float blendSpeed = 1f;
-
 
     private void Awake() {
 
@@ -19,7 +16,6 @@ public class BookG : MonoBehaviour {
 
     // Use this for initialization
     void Start() {
-        smr = GetComponentInChildren<SkinnedMeshRenderer>();
         animator = GetComponent<Animator>();
     }
 
