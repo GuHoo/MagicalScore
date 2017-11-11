57c66383f646f6e735b0da0a60f3bdf262aa81f5
Nakai Tomoki <s16g459@stu.kagawa-u.ac.jp>
2017-11-11 17:53:45 +0900

    いい感じに回転させるスクリプト

---
+++ Assets/Scripts/BornBook.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BornBook : MonoBehaviour {
+
+    bool continueFlag = true;
+    float time = 0.0f;
+    float changedAngle = 0f;
+    private Animator animator;
+
+    float minAngleX = 0.0F;
+    float maxAngleX = 90.0F;
+
+    float minAngleZ = 90.0F;
+    float maxAngleZ = 0.0F;
+
+    void Start () {
+        animator = GetComponent<Animator>();
+        //animator.SetBool("isInitialize", true);
+    }
+	
+	void Update () {
+        if (continueFlag) {
+            float angleX = Mathf.LerpAngle(minAngleX, maxAngleX, Time.time);
+            float angleZ = Mathf.LerpAngle(minAngleZ, maxAngleZ, Time.time);
+            transform.eulerAngles = new Vector3(angleX, 180, angleZ);
+
+
+            /*
+            transform.Rotate(new Vector3(0, -60f * Time.deltaTime, -60f * Time.deltaTime));
+            changedAngle += 60f * Time.deltaTime;
+            */
+            if (changedAngle >= 90f)
+            {
+                continueFlag = false;
+            }
+        }
+	}
+}
